b8f5b2e7bff2123c27a56eba2245a999eab83e20
Drew Myers <drew.myers59@gmail.com>
2022-09-02 15:02:50 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0.00;
+        public BankAccount()
+        {
+        }
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BankAccountExercise
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount checking = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit into checkings?");
+            double amount = double.Parse(Console.ReadLine());
+
+            checking.Deposit(amount);
+            double newBalance = checking.GetBalance();
+
+            Console.WriteLine($"Thank you for your deposit. Your account now has ${newBalance} in checkings.");
+        }
+    }
+}
